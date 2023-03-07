cf0137f2ff20c4208fe5402886d459dc90dd4268
leesongun <12179851+leesongun@users.noreply.github.com>
2023-03-08 04:24:16 +0900

    workaround to implementing sized hermitian matrix

--- src/Jordan.jl
+++ src/Jordan.jl
@@ -18,28 +18,20 @@ Base.promote_rule(::Type{Octonion{T}}, ::Type{S}) where {T<:Real,S<:Real} = Octo
 Base.promote_rule(::Type{Octonion{T}}, ::Type{Octonion{S}}) where {T<:Real,S<:Real} = Octonion{promote_type(T, S)}
 
 Base.:-(o::Octonion) = Octonion(-o.v)
+Base.real(o::Octonion) = o.v[1]
+Base.conj(o::Octonion) = Octonion(vcat(o.v[1], -o.v[2:8]))
 
 Base.:+(o1::Octonion, o2::Octonion) = Octonion(o1.v + o2.v)
 Base.:-(o1::Octonion, o2::Octonion) = Octonion(o1.v - o2.v)
-Base.conj(o::Octonion) = Octonion(vcat(o.v[1], -o.v[2:8]))
 
 # Warning: not numerically best way to implement!
 function Base.:*(o1::Octonion, o2::Octonion)::Octonion
-    i1::SizedVector{7} = o1.v[2:8]
-    i2::SizedVector{7} = o2.v[2:8]
-    rotmul(s) = circshift(i1, s) .* i2 - i1 .* circshift(i2, s)
-    mul = sum(circshift(rotmul(a), 2 * a) for a in [1, 2, 4])
-    ret = o1.v[1] * o2.v + vcat(-i1'i2, i1 * o2.v[1] + mul)
-    return Octonion(ret)
+    i1, i2 = o1.v[2:8], o2.v[2:8]
+    rotmul(s) = circshift(circshift(i1, s) .* i2 - i1 .* circshift(i2, s), 2 * s)
+    return Octonion(o1.v[1] * o2.v + vcat(-i1'i2, i1 * o2.v[1] + sum(rotmul.([1, 2, 4]))))
 end
 
 Base.:(==)(o1::Octonion, o2::Octonion) = o1.v == o2.v
 Base.isequal(o1::Octonion, o2::Octonion) = isequal(o1.v, o2.v)
-
-struct Albert{T}
-    m::SMatrix{Octonion{T},3,3,9}
-    # inner constructor to enforce adjoint condition
-end
-Base.:*(a1::Albert, a2::Albert)::Albert = (a1.m * a2.m + a2.m * a1.m) / 2
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using Jordan
 using Test
 using Symbolics
 
+makeOct(x) = Octonion([Symbolics.scalarize(x[i]) for i in 1:8])
+
 function Oct(x, y)
     z = (x - y).v
     return all(isequal(simplify(z[i]; expand=true), 0) for i in 1:8)
@@ -9,7 +11,7 @@ end
 
 @testset "Octonions" begin
     @variables x[1:3, 1:8]
-    (a, b, c) = [Octonion([Symbolics.scalarize(x[i, j]) for j in 1:8]) for i in 1:3]
+    a, b, c = [makeOct(x[i, :]) for i in 1:3]
 
     @test Oct(a, conj(conj(a)))
 
@@ -31,29 +33,16 @@ end
     @test Oct((a * b) * (c * a), a * ((b * c) * a)) # Moufang identity. redundant by definition and flexibility.
 end
 
-function MakeAlbert(x, i, j)
-    if i < j
-        return Octonion([Symbolics.scalarize(x[6-i-j, k]) for k in 1:8])
-    elseif i == j
-        return Octonion(x[i, 9])
-    else
-        return conj(MakeAlbert(x, j, i))
-    end
-end
 
-function MakeMatrix(x)::Matrix
-    return reshape([MakeAlbert(x, i, j) for (i, j) in Iterators.product(1:3, 1:3)], (3, 3))
-end
+using LinearAlgebra
 
-function Alb(x, y)
-    t(a) = Oct(a, 0)
-    return all(t.(x - y))
-end
+Alb(x, y) = all(Oct.(x - y, 0))
+makeMatrix(x) =  Hermitian(reshape([makeOct(x[i, :]) for i in 1:9], (3, 3)))
 
 @testset "Albert" begin
-    @variables x[1:2, 1:3, 1:9]
+    @variables x[1:2, 1:9, 1:9]
 
-    (a, b) = [MakeMatrix(x[i, :, :]) for i in 1:2]
+    a, b = [makeMatrix(x[i, :, :]) for i in 1:2]
     t(x, y) = (x * y + y * x)# / 2
     @test Alb(t(a, b), t(b, a))
     @test Alb(t(t(a, b), t(a, a)), t(a, t(b, t(a, a))))
